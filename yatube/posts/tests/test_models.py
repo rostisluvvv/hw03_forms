b11235440180314b07bfbca67b7d894b31a4edaa
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-02-13 08:45:17 +0300

    test_models

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,1 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.db.models import get_app, get_models
 from django.test import TestCase
+
+from ..models import Post, Group
+
+
+User = get_user_model()
+
+
+class PostModelTest(TestCase):
+    """Class testing post model"""
+
+    @classmethod
+    def setUp(cls) -> None:
+        super().setUpClass()
+
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание'
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост'
+        )
+
+    def test_models_have_correct_object_names(self):
+        """Проверяем, что у моделей корректно работает __str__."""
+        group = PostModelTest.group
+        self.assertEqual(str(group.title), group.title)
